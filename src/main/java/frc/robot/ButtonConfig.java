c578089ac60a541e594c97f7243f8d594bca84c5
AlexanderGardiner <misc@alexandergardiner.com>
2025-01-31 17:52:58 -0800

    Score command (temp)

--- src/main/java/frc/robot/ButtonConfig.java
+++ src/main/java/frc/robot/ButtonConfig.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.Commands.ReefSelection.ReefLevelSelection;
 import frc.robot.Commands.ReefSelection.SetOrientation;
+import frc.robot.Commands.complex.ScoreCoral;
+import frc.robot.Subsystems.Elevator.Elevator;
 import frc.robot.Subsystems.Swerve.Swerve;
 
 public class ButtonConfig {
@@ -23,7 +25,6 @@ public class ButtonConfig {
                 () -> {
                   Swerve.getInstance().zeroGyro();
                 }));
-  
 
     coDriverLeft.button(1).onTrue(new SetOrientation(0));
     coDriverLeft.button(2).onTrue(new SetOrientation(1));
@@ -31,5 +32,8 @@ public class ButtonConfig {
     coDriverButtons.button(7).onTrue(new ReefLevelSelection(2));
     coDriverButtons.button(9).onTrue(new ReefLevelSelection(1));
     coDriverButtons.button(11).onTrue(new ReefLevelSelection(0));
+
+    driverRight.button(1).whileTrue(new ScoreCoral(Elevator.getInstance().getReefTargetLevel(),
+        Swerve.getInstance().getReefTargetSide(), Swerve.getInstance().getReefTargetOrientation()));
   }
 }
