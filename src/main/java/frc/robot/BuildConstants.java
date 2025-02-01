45918a89f43a46d81c2057946f4552b885df1531
Oliver Hakansson <theoliverhakansson@gmail.com>
2025-01-31 18:08:06 -0800

    working reefselection code with codriver joystick

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 105;
-  public static final String GIT_SHA = "ff65ceca34f30e4848e1f4965c87ff7fde704ea9";
-  public static final String GIT_DATE = "2025-01-27 23:46:54 EST";
+  public static final int GIT_REVISION = 109;
+  public static final String GIT_SHA = "3b348a5a69e38b8e61ad8224c6df97c35b260a59";
+  public static final String GIT_DATE = "2025-01-31 20:44:09 EST";
   public static final String GIT_BRANCH = "Subsystems-Development";
-  public static final String BUILD_DATE = "2025-01-29 22:06:44 EST";
-  public static final long BUILD_UNIX_TIME = 1738206404013L;
+  public static final String BUILD_DATE = "2025-01-31 20:51:43 EST";
+  public static final long BUILD_UNIX_TIME = 1738374703912L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
