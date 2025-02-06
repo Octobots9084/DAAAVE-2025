f39ed67c4713a65dc69579659e67d46ef4db86b3
Michael Shinn <michaelstevenshinn@gmail.com>
2025-02-05 19:11:54 -0800

    import fix

--- src/main/java/frc/robot/ButtonConfig.java
+++ src/main/java/frc/robot/ButtonConfig.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import org.littletonrobotics.junction.Logger;
 
+import com.revrobotics.spark.ClosedLoopSlot;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
