868b6be070fc4ed70a43c7512baed8dab8544cdb
Sinjin <sinjin@mongold.net>
2025-02-08 12:56:41 -0800

    Added Climb subsystem

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 109;
-  public static final String GIT_SHA = "3b348a5a69e38b8e61ad8224c6df97c35b260a59";
-  public static final String GIT_DATE = "2025-01-31 20:44:09 EST";
+  public static final int GIT_REVISION = 165;
+  public static final String GIT_SHA = "71370f51d0b63cac85c5adb7eccd609aa61d12a3";
+  public static final String GIT_DATE = "2025-02-03 23:38:31 EST";
   public static final String GIT_BRANCH = "Subsystems-Development";
-  public static final String BUILD_DATE = "2025-01-31 20:51:43 EST";
-  public static final long BUILD_UNIX_TIME = 1738374703912L;
+  public static final String BUILD_DATE = "2025-02-05 22:46:42 EST";
+  public static final long BUILD_UNIX_TIME = 1738813602590L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
