4c2ba242a4aa81d62e92157cc4b4cceb40942a01
lyyleem <laurenlee4655@gmail.com>
2025-02-14 13:11:42 -0800

    More Fixes

--- src/main/java/frc/robot/Commands/complex/collectcoral/WaitForClawBackSensor.java
+++ src/main/java/frc/robot/Commands/complex/collectcoral/WaitForClawBackSensor.java
@@ -1,4 +1,4 @@
-package frc.robot.Commands.complex.collectcoral;
+package frc.robot.Commands.complex.collectCoral;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
