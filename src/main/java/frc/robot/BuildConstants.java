5c9f3ad8751c67cb26fe5253d871970854ea62ed
AlexanderGardiner <misc@alexandergardiner.com>
2025-02-17 14:46:43 -0800

    Updated climb code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 249;
-  public static final String GIT_SHA = "be2b2ee05f20890ff8c654140fe352ca48d1011c";
-  public static final String GIT_DATE = "2025-02-16 18:39:20 GMT-05:00";
+  public static final int GIT_REVISION = 254;
+  public static final String GIT_SHA = "6e546cd1d0af9f4e3aa74ee8ce3363c792533cd7";
+  public static final String GIT_DATE = "2025-02-17 15:11:36 GMT-05:00";
   public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-02-16 19:33:37 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1739752417193L;
+  public static final String BUILD_DATE = "2025-02-17 15:19:31 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1739823571943L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
