ba9b0aad7ba5c6b72b48ed0ef56456167cc2dea5
lyyleem <laurenlee4655@gmail.com>
2025-02-18 21:03:06 -0800

    updated autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 255;
-  public static final String GIT_SHA = "5c9f3ad8751c67cb26fe5253d871970854ea62ed";
-  public static final String GIT_DATE = "2025-02-17 17:46:43 GMT-05:00";
+  public static final int GIT_REVISION = 261;
+  public static final String GIT_SHA = "528c5c7ad5d5664e3a61f17aaa110a4c6ea5f67c";
+  public static final String GIT_DATE = "2025-02-18 16:02:18 EST";
   public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-02-17 19:43:37 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1739839417104L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-18 23:33:35 EST";
+  public static final long BUILD_UNIX_TIME = 1739939615465L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
