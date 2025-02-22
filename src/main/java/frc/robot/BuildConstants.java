cef28993a6f9603fd8d08b83ea0281408552e1df
AlexanderGardiner <misc@alexandergardiner.com>
2025-02-22 13:09:45 -0800

    Slew rate updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 271;
-  public static final String GIT_SHA = "25dd8226109ab518a98efacd6d269c1d1c59a664";
-  public static final String GIT_DATE = "2025-02-22 13:54:37 GMT-05:00";
+  public static final int GIT_REVISION = 272;
+  public static final String GIT_SHA = "f28bf673496a31e741fdf71993c9d39f1bef4517";
+  public static final String GIT_DATE = "2025-02-22 15:43:37 GMT-05:00";
   public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-02-22 15:00:38 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1740254438082L;
+  public static final String BUILD_DATE = "2025-02-22 16:07:40 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1740258460498L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
