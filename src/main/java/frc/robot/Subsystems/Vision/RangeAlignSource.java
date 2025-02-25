c8dcb9be1e1b44036c2d8aa20d7bdbf4b5e08bcd
Aayush Kokate <aayushext@outlook.com>
2025-02-24 19:08:55 -0800

    Added CANrange configs...just in case

--- src/main/java/frc/robot/Subsystems/Vision/RangeAlignSource.java
+++ src/main/java/frc/robot/Subsystems/Vision/RangeAlignSource.java
@@ -1,5 +1,7 @@
 package frc.robot.Subsystems.Vision;
 
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.FovParamsConfigs;
 import com.ctre.phoenix6.hardware.CANrange;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -9,6 +11,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class RangeAlignSource extends SubsystemBase {
     CANrange backRange;
     PIDController backRangePID;
+    CANrangeConfiguration configuration;
+    FovParamsConfigs paramsConfigs;
 
     private static RangeAlignSource instance;
 
@@ -22,6 +26,17 @@ public class RangeAlignSource extends SubsystemBase {
     public RangeAlignSource() {
         this.backRange = new CANrange(0, "KrakensBus");
         this.backRangePID = new PIDController(2, 0, 0);
+
+        this.paramsConfigs = new FovParamsConfigs();
+        paramsConfigs.withFOVRangeX(6.75);
+        paramsConfigs.withFOVRangeY(6.75);
+        paramsConfigs.withFOVCenterX(6.75);
+        paramsConfigs.withFOVCenterY(6.75);
+
+        this.configuration = new CANrangeConfiguration();
+        configuration.withFovParams(paramsConfigs);
+        configuration.ProximityParams.ProximityThreshold = 1;
+        backRange.getConfigurator().apply(configuration);
     }
 
     public double getBackRange() {
