57585d72cabc259248ab8c3eabfe3b11d5a4842e
jcorr <jarrett@corr.org>
2025-03-04 22:14:18 -0800

    some changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,16 +4,16 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "Placeholder-2025";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 384;
-    public static final String GIT_SHA = "1bd6a9dc69de1e151c479a8bc8d51dac1343cc20";
-    public static final String GIT_DATE = "2025-03-03 23:28:17 EST";
-    public static final String GIT_BRANCH = "Development";
-    public static final String BUILD_DATE = "2025-03-03 23:56:15 EST";
-    public static final long BUILD_UNIX_TIME = 1741064175625L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Placeholder-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 388;
+  public static final String GIT_SHA = "b28b4289e71cc1a43af8d54485f2fe848b06880f";
+  public static final String GIT_DATE = "2025-03-04 01:42:22 EST";
+  public static final String GIT_BRANCH = "reverting_logging_changes";
+  public static final String BUILD_DATE = "2025-03-05 01:13:46 EST";
+  public static final long BUILD_UNIX_TIME = 1741155226018L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants() {}
+  private BuildConstants(){}
 }
--- src/main/java/frc/robot/Subsystems/Wrist/WristStates.java
+++ src/main/java/frc/robot/Subsystems/Wrist/WristStates.java
@@ -2,20 +2,21 @@ package frc.robot.Subsystems.Wrist;
 
 public enum WristStates {
     // TODO edit these to the actual positions
-    INTAKE(0.435),
-    ELEPHANTIASIS(0.55),
-    L1(0.587),
-    L2(0.92),
-    L3(0.91),
-    L4(0.820),
-    PREP(0.97),
-    MANUAL(0.47),
-    ALAGEREMOVAL(0.757),
-    ALGAEPOP(0.757),
-    ALAGESTACKREMOVAL(0.7),
-    TUNING(0.7561);// TODO actually set this value
-    // TODO actually set this value
-
+    INTAKE(0.375),
+    ELEPHANTIASIS(0.40),
+    L1(0.547),
+    L2(0.828),
+    L3(0.828),
+    L4(0.745),
+    PREP(0.92),
+    MANUAL(0.45),
+    ALGAEREMOVAL(0.828),
+    ALAGESTACKREMOVAL(0.610),
+    BARGEALGAE(0.90),
+    TUNING(0.7561), // TODO actually set this value
+    TOPALGAEINTAKE(00000000),
+    BOTTOMALGAEINTAKE(00000000);
+    
     public double wristPosition;
 
     WristStates(double totalPosition) {
