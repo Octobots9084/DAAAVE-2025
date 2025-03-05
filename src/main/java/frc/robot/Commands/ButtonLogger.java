258d35509728e344f5aa3f3b570caa44433ddcaf
jcorr <jarrett@corr.org>
2025-03-04 19:41:59 -0800

    made button logger more legible

--- src/main/java/frc/robot/Commands/ButtonLogger.java
+++ src/main/java/frc/robot/Commands/ButtonLogger.java
@@ -6,6 +6,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 public class ButtonLogger extends InstantCommand{
 
     public ButtonLogger (String log, boolean driver, int buttonNumber) {
-        Logger.recordOutput(("Button " + buttonNumber + ", Driver? " + driver), log);
+        if (driver) {
+            Logger.recordOutput(("Driver pressed button " + buttonNumber), log);
+        } else {
+            Logger.recordOutput(("Co-Driver pressed button " + buttonNumber), log);
+        }
     }
 }
