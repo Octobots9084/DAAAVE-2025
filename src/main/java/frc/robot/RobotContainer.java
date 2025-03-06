55b4e0bfef9d4596bc96a8d8b70d1eadf1d3bbc9
jcorr <jarrett@corr.org>
2025-03-05 17:32:05 -0800

    Updated wrist state positions, updated driver stick scaling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -158,11 +158,11 @@ public class RobotContainer {
 
         TeleopDrive closedFieldRel = new TeleopDrive(
                 () -> Math.pow(MathUtil.applyDeadband(
-                        negative * ButtonConfig.driverLeft.getRawAxis(1), OperatorConstants.LEFT_Y_DEADBAND) / 2.,3.0),
+                        negative * ButtonConfig.driverLeft.getRawAxis(1), OperatorConstants.LEFT_Y_DEADBAND),3),
                 () -> Math.pow(MathUtil.applyDeadband(
-                        negative * ButtonConfig.driverLeft.getRawAxis(0), OperatorConstants.LEFT_X_DEADBAND) / 2.,3),
+                        negative * ButtonConfig.driverLeft.getRawAxis(0), OperatorConstants.LEFT_X_DEADBAND),3),
                 () -> MathUtil.applyDeadband(
-                        -ButtonConfig.driverRight.getRawAxis(0), OperatorConstants.RIGHT_X_DEADBAND) / 3.);
+                        -ButtonConfig.driverRight.getRawAxis(0), OperatorConstants.RIGHT_X_DEADBAND) / 2.0);
         swerve.setDefaultCommand(closedFieldRel);
 
         autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
