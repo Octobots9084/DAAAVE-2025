78f7853916283c9ad55fcd960d194d918efb49b4
bluefury6 <bluefury6@gmail.com>
2025-03-18 21:07:10 -0700

    Happy noises

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 492;
-  public static final String GIT_SHA = "9d7bd2092c45ccdd63c9599d9cd8bda86ab948f3";
-  public static final String GIT_DATE = "2025-03-18 21:38:55 EDT";
+  public static final int GIT_REVISION = 493;
+  public static final String GIT_SHA = "f3b6513f80605d0815c7a6515c6c1ca2b14bc8ff";
+  public static final String GIT_DATE = "2025-03-18 22:13:56 EDT";
   public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-03-18 22:11:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1742350266592L;
+  public static final String BUILD_DATE = "2025-03-18 23:59:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1742356770734L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/Subsystems/Vision/AlignVision.java
+++ src/main/java/frc/robot/Subsystems/Vision/AlignVision.java
@@ -129,7 +129,7 @@ public class AlignVision extends SubsystemBase {
         this.cameraXPIDController = new PIDController(2, 0, 0);
         this.backCameraXPIDController = new PIDController(3.5, 0, 0);
 
-        this.cameraYPIDController = new PIDController(2.5, 0, 0);
+        this.cameraYPIDController = new PIDController(2.75, 0, 0);
         this.cameraYPIDControllerSource = new PIDController(2, 0, 0);
         
         this.lidarXPIDController = new PIDController(4, 0, 0);
