e419198c9f66597f32af9a20766fd4f568bab26e
AlexanderGardiner <misc@alexandergardiner.com>
2025-03-19 18:47:24 -0700

    yagsl update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 493;
-  public static final String GIT_SHA = "f3b6513f80605d0815c7a6515c6c1ca2b14bc8ff";
-  public static final String GIT_DATE = "2025-03-18 22:13:56 EDT";
+  public static final int GIT_REVISION = 494;
+  public static final String GIT_SHA = "78f7853916283c9ad55fcd960d194d918efb49b4";
+  public static final String GIT_DATE = "2025-03-19 00:07:10 GMT-04:00";
   public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-03-18 23:59:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1742356770734L;
+  public static final String BUILD_DATE = "2025-03-19 21:35:43 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1742434543599L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
