d6e1c295e40de5808894eff9dc516acb117f8ae0
AlexanderGardiner <misc@alexandergardiner.com>
2025-03-21 13:39:23 -0700

    Auto fixes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Placeholder-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 499;
-  public static final String GIT_SHA = "e011c9a19435ccada5f69235e0eb3ce885b13b01";
-  public static final String GIT_DATE = "2025-03-19 23:26:54 GMT-04:00";
-  public static final String GIT_BRANCH = "Development";
-  public static final String BUILD_DATE = "2025-03-19 23:59:41 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1742443181028L;
+  public static final int GIT_REVISION = 504;
+  public static final String GIT_SHA = "d573b7b6234d497ed072c1f3fa38d78f91906755";
+  public static final String GIT_DATE = "2025-03-20 00:03:44 GMT-04:00";
+  public static final String GIT_BRANCH = "San-Diego";
+  public static final String BUILD_DATE = "2025-03-21 16:06:02 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1742587562991L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
