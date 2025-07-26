802e2f414305af18406dc2e0ba10ab2ad2fb1c3b
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2025-07-25 19:25:34 -0700

    im scared

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "DAAAVE-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 536;
-  public static final String GIT_SHA = "ae7f02b3ac56b73e219bf9298bb63534544e0dff";
-  public static final String GIT_DATE = "2025-07-15 01:06:01 EDT";
-  public static final String GIT_BRANCH = "Chezy-Champs-2025";
-  public static final String BUILD_DATE = "2025-07-21 22:23:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1753151019871L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 537;
+  public static final String GIT_SHA = "a987ae1609106ca5c7bc665b5706be5b8b7c483d";
+  public static final String GIT_DATE = "2025-07-21 22:30:54 EDT";
+  public static final String GIT_BRANCH = "New-Chezy-Champs-2025";
+  public static final String BUILD_DATE = "2025-07-25 22:22:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1753496557273L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/Commands/complex/BargeThrow.java
@@ -0,0 +1,24 @@
+package frc.robot.Commands.complex;
+
+import com.revrobotics.spark.ClosedLoopSlot;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Commands.CoralRollers.SetCoralRollersState;
+import frc.robot.Commands.Elevator.SetElevatorState;
+import frc.robot.Commands.Wrist.SetWristState;
+import frc.robot.Subsystems.CoralRollers.CoralRollersState;
+import frc.robot.Subsystems.Elevator.ElevatorStates;
+import frc.robot.Subsystems.Wrist.WristStates;
+
+public class BargeThrow extends SequentialCommandGroup{
+    public BargeThrow(){
+        addCommands(
+            new SetWristState(WristStates.PREP, ClosedLoopSlot.kSlot0),
+            new SetElevatorState(ElevatorStates.LEVEL4),       
+            new WaitCommand(0.7),
+            new SetWristState(WristStates.BARGEALGAE,ClosedLoopSlot.kSlot0),
+            new SetCoralRollersState(CoralRollersState.AlGAEOUTPUT)
+        );
+    }
+}
