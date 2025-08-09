5f067dd12af5c197e61c189ba28de7521682c127
Oliver Hakansson <153968273+OliverHakansson@users.noreply.github.com>
2025-08-08 19:27:36 -0700

    auto climb?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "DAAAVE-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 552;
-  public static final String GIT_SHA = "fac0d204e8949807f1b036bcf8525d9095ce4be9";
-  public static final String GIT_DATE = "2025-08-04 23:28:16 EDT";
+  public static final int GIT_REVISION = 553;
+  public static final String GIT_SHA = "a1ed37f505105bb5aa99c9ad4722407b67aa004f";
+  public static final String GIT_DATE = "2025-08-04 23:50:50 EDT";
   public static final String GIT_BRANCH = "Climb-Limit-Switches";
-  public static final String BUILD_DATE = "2025-08-04 23:48:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1754365695292L;
+  public static final String BUILD_DATE = "2025-08-08 22:15:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1754705733957L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/Commands/complex/deployClimb.java
+++ src/main/java/frc/robot/Commands/complex/deployClimb.java
@@ -5,12 +5,13 @@ import com.revrobotics.spark.ClosedLoopSlot;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.States.ReefTargetSide;
 import frc.robot.Commands.Climb.RunClimbRollers;
 import frc.robot.Commands.Climb.SetClimbState;
 import frc.robot.Commands.ReefSelection.ReefLevelSelection;
 import frc.robot.Commands.Wrist.SetWristState;
+import frc.robot.Subsystems.Climb.Climb;
 import frc.robot.Subsystems.Climb.ClimbStates;
 import frc.robot.Subsystems.Vision.AlignVision;
 import frc.robot.Subsystems.Wrist.WristStates;
@@ -18,12 +19,13 @@ import frc.robot.Subsystems.Wrist.WristStates;
 public class deployClimb extends SequentialCommandGroup{
     public deployClimb(){
         addCommands(
-                    //20
-                new SetWristState(WristStates.ALGAEREMOVAL, ClosedLoopSlot.kSlot0),
-                new RunClimbRollers(), 
-                    
-                //16
-               new SetClimbState(ClimbStates.Deployed, ClosedLoopSlot.kSlot1));
+            new SetWristState(WristStates.ALGAEREMOVAL, ClosedLoopSlot.kSlot0),
+            new RunClimbRollers(), 
+            new SetClimbState(ClimbStates.Deployed, ClosedLoopSlot.kSlot1),
+            new WaitUntilCommand(() -> Climb.getInstance().isClimbClamped()),
+            new WaitUntilCommand(3),
+            new SetClimbState(ClimbStates.Climbing, ClosedLoopSlot.kSlot0)
+        ); 
         }
                 
     }
